1ab9506d5ae7c3a7b92cb02571b73f84f0f6f6d2
Adwaith CD <adwaithgowda@gmail.com>
2024-11-04 14:09:21 -0700

    Updated chatbot ui references to educhat

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -12,10 +12,10 @@ import { ReactNode } from "react"
 import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
-const APP_NAME = "Chatbot UI"
-const APP_DEFAULT_TITLE = "Chatbot UI"
-const APP_TITLE_TEMPLATE = "%s - Chatbot UI"
-const APP_DESCRIPTION = "Chabot UI PWA!"
+const APP_NAME = "EDUCHAT"
+const APP_DEFAULT_TITLE = "EDUCHAT"
+const APP_TITLE_TEMPLATE = "%s - EDUCHAT"
+const APP_DESCRIPTION = "EDUCHAT UI PWA!"
 
 interface RootLayoutProps {
   children: ReactNode
--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -4,6 +4,8 @@ import { ChatbotUISVG } from "@/components/icons/chatbotui-svg"
 import { IconArrowRight } from "@tabler/icons-react"
 import { useTheme } from "next-themes"
 import Link from "next/link"
+//@ts-ignore
+import { UilRobot } from "@iconscout/react-unicons"
 
 export default function HomePage() {
   const { theme } = useTheme()
@@ -11,10 +13,12 @@ export default function HomePage() {
   return (
     <div className="flex size-full flex-col items-center justify-center">
       <div>
-        <ChatbotUISVG theme={theme === "dark" ? "dark" : "light"} scale={0.3} />
+        {/* <ChatbotUISVG theme={theme === "dark" ? "dark" : "light"} scale={0.3} /> */}
+        <UilRobot size={80} />
       </div>
 
-      <div className="mt-2 text-4xl font-bold">Chatbot UI</div>
+      {/* <div className="mt-2 text-4xl font-bold">Chatbot UI</div> */}
+      <div className="mt-2 text-4xl font-bold">EDUCHAT</div>
 
       <Link
         className="mt-4 flex w-[200px] items-center justify-center rounded-md bg-blue-500 p-2 font-semibold"
