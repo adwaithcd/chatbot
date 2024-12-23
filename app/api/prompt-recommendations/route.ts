157216a44cfb694fe31b1f6616a5beb8bdc5c6af
Adwaith CD <adwaithgowda@gmail.com>
2024-12-23 20:46:48 +0530

    Added APPLICATION_CHATBOT_RECOMMENDATIONS_URL as env parameter, updated recommendations api to use this env param

--- app/api/prompt-recommendations/route.ts
+++ app/api/prompt-recommendations/route.ts
@@ -1,25 +1,29 @@
 import { NextResponse } from "next/server"
 
+const APPLICATION_CHATBOT_RECOMMENDATIONS_URL =
+  process.env.APPLICATION_CHATBOT_RECOMMENDATIONS_URL
+
 export async function POST(req: Request) {
   try {
+    if (!APPLICATION_CHATBOT_RECOMMENDATIONS_URL) {
+      throw new Error("APPLICATION_CHATBOT_RECOMMENDATIONS_URL is not defined")
+    }
+
     const { message } = await req.json()
     const promptText = message
       ? message
       : "Give some sample questions which might be useful to a student for college application?"
-    const response = await fetch(
-      "http://demo-d-Publi-NU2RRqsuaVm7-500183766.us-east-1.elb.amazonaws.com/prompt_recs/invoke",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          input: {
-            question: promptText
-          }
-        })
-      }
-    )
+    const response = await fetch(APPLICATION_CHATBOT_RECOMMENDATIONS_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        input: {
+          question: promptText
+        }
+      })
+    })
 
     if (!response.ok) {
       throw new Error("Failed to fetch recommendations")
